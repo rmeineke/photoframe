271a99342c4b54239ddf506cf24bd438aa28402f
Robert Meineke <rmeineke@gmail.com>
2018-12-06 02:50:38 -0800

    updating gitignore

--- firsttry.py
+++ firsttry.py
@@ -21,19 +21,41 @@ def get_longest_side(aspect):
         return 'x'
 
 
+def resize_image(filename):
+    print(f'inside resize_image: {filename}')
+    outfile = 'sm_' + filename
+    print(f'outfile: {outfile}')
+    size = (800, 600)
+    try:
+        im = Image.open(os.path.join('input', filename))
+        im.thumbnail(size, Image.ANTIALIAS)
+        return im
+    except IOError:
+        print("cannot create thumbnail for " + {filename})
+
+
+def composite_image(infile):
+    bg = Image.open('background.png')
+    in_x, in_y = infile.size
+    bg.paste(infile, ( int((800 - in_x) / 2) , int((600 - in_y) / 2)) )
+    return bg
+
+
 for f in os.listdir('input'):
     print(f'{f}')
+    file_stub, file_ext = os.path.splitext(f)
     filename = os.path.join('input', f)
     print(f'{os.path.abspath(filename)}')
-    im = Image.open(f'{filename}')
+    resized = resize_image(f)
 
-    aspect = get_aspect(im)
-    print(f'{aspect}')
+    resized = resized.convert('RGB')
+    sm_file = f"{os.path.join('resized', 'sm_' + f)}"
+    resized.save(sm_file, "JPEG")
 
-    longest_side = get_longest_side(aspect)
-    print(f'longest_side: {longest_side}')
-
-
+    composited = composite_image(resized)
+    final_name = os.path.join('output', file_stub + '.jpg')
+    print(f'{final_name}')
+    composited.save(final_name, "JPEG")
 # 800x600
 # jpg, avi
 # ttl 1GB
