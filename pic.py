22875628bf2e58514fd9a2056c59b3cf5c79b9a1
Robert Meineke <rmeineke@gmail.com>
2018-12-06 04:32:54 -0800

    oop ... works ... and cleaned up a bit

--- pic.py
+++ pic.py
@@ -4,23 +4,17 @@ import os
 class Pic:
     def __init__(self, f):
         self.__filename = f
+
         self.__file_base_name = None
-        self.__file_ext = None
+        self.__file_base_name, _ = os.path.splitext(self.__filename)
 
-        self.__full_file_name = os.path.abspath(f)
-        self.__file_base_name, self.__file_ext = os.path.splitext(self.__filename)
-
-        self.__output_file = os.path.join('output' + self.__file_base_name + '.jpg')
-
+        self.__input_filename = os.path.join('input', f)
+        self.__output_file = os.path.join('output', self.__file_base_name + '.jpg')
 
     @property
-    def filename(self):
-        return self.__full_file_name
+    def input_filename(self):
+        return self.__input_filename
 
     @property
-    def ext(self):
-        return self.__file_ext
-
-    @property
-    def stub(self):
-        return self.__file_base_name
+    def output_file(self):
+        return self.__output_file
